46a9fc3321c2aa953b96741485837fb3d1ca3e81
mayrars <mayra203@gmail.com>
2024-07-25 07:02:45 -0600

    adding new routes pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PokemonComponent } from './pages/pokemon/pokemon.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pokemon/:name', component: PokemonComponent }
+];
