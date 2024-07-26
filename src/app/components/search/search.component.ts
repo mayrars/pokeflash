3e80c69bff14af30b353fe5f698f1aea0df16f6f
mayrars <mayra203@gmail.com>
2024-07-26 07:05:47 -0600

    Created modal for error

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Pokemon } from '../../interfaces/pokemon.interface';
 import { Pokemons } from '../../interfaces/pokemons.interface';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PokemonService } from '../../services/pokemon.service';
 import { NgClass } from '@angular/common';
+import bootstrap from '../../../main.server';
 
 @Component({
   selector: 'app-search',
@@ -14,16 +15,16 @@ import { NgClass } from '@angular/common';
 })
 export class SearchComponent{
   searchForm!: FormGroup
+  @ViewChild("#exampleModal") modal?: ElementRef;
   constructor(private formBuilder: FormBuilder) {
     this.searchForm= this.formBuilder.group({
       search: ['',[Validators.required,Validators.minLength(3)]]
     })
   }
   searchByName(event:any){
-    //console.log('here')
     const value = event.target.value;
     if(value==null || value==''){
-      console.log("sin resultados")
+      this.modal
     }else{
 
     }
