77ca734350203346c6dee466120e15db83db5732
mayrars <mayra203@gmail.com>
2024-07-31 07:05:45 -0600

    change card component for horizontal and vertical card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,18 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Pokemon } from '../../interfaces/pokemon.interface';
 import { PokemonService } from '../../services/pokemon.service';
-import { TitleCasePipe } from '@angular/common';
+import { NgOptimizedImage, TitleCasePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [TitleCasePipe, RouterLink],
+  imports: [TitleCasePipe, RouterLink,NgOptimizedImage],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent implements OnInit{
   @Input() poke: any;
+  @Input() position: string='';
   pokemon?:Pokemon;
   constructor( private apiService: PokemonService){}
   ngOnInit(): void {
