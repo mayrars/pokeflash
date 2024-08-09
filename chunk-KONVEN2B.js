b6b9af288e1be4f037e1b11c453ba90f67baa8c7
mayrars <mayra203@gmail.com>
2024-08-09 07:09:10 -0600

    Auto-generated commit

--- chunk-KONVEN2B.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./chunk-OMNEVETU.js";import"./chunk-4I7HX3XZ.js";export{a as CardComponent};
---
+++ chunk-VN7EHXZA.js
@@ -0,0 +1,1 @@
+import{i as a}from"./chunk-S4TIC3JJ.js";import"./chunk-4I7HX3XZ.js";export{a as CardComponent};
