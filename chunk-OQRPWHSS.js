dde9609b57397bd0362fe20d503eb399b520ad5b
mayrars <mayra203@gmail.com>
2024-08-14 07:05:39 -0600

    Auto-generated commit

--- chunk-OQRPWHSS.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./chunk-23YHCJUL.js";import"./chunk-F745MT6I.js";export{a as CardComponent};
---
+++ chunk-TTMHLCWP.js
@@ -0,0 +1,1 @@
+import{i as a}from"./chunk-JYMTM3FS.js";import"./chunk-F745MT6I.js";export{a as CardComponent};
