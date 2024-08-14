b2a1067492f9f5ea8d635e6206a250f383831995
mayrars <mayra203@gmail.com>
2024-08-14 07:06:52 -0600

    validate image and adding loader

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Pokemon } from '../../interfaces/pokemon.interface';
 import { PokemonService } from '../../services/pokemon.service';
 import { NgOptimizedImage, TitleCasePipe } from '@angular/common';
@@ -11,17 +11,28 @@ import { RouterLink } from '@angular/router';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent implements OnInit{
+export class CardComponent implements OnInit, OnChanges{
   @Input() poke: any;
   @Input() position: string='';
   pokemon?: Pokemon = {} as Pokemon;
+  loader: boolean = true;
   constructor( private apiService: PokemonService){}
   ngOnInit(): void {
+    this.pokemon = {} as Pokemon;
     this.apiService.getSpeciesPokemon(this.poke.url).subscribe(
       (data) => {
-          this.pokemon = {} as Pokemon;
-          this.pokemon = data
-        }
+        this.pokemon = data
+        this.loader= false
+      }
+    )
+  }
+  ngOnChanges(): void {
+    this.loader = true
+    this.apiService.getSpeciesPokemon(this.poke.url).subscribe(
+      (data) => {
+        this.pokemon = data
+        this.loader= false
+      }
     )
   }
 }
