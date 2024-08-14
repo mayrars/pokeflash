9ed3216a64fb34d4ffb385fdbc020ca8c31c07e1
mayrars <mayra203@gmail.com>
2024-08-14 07:05:13 -0600

    created berries page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PokemonComponent } from './pages/pokemon/pokemon.component';
+import { BerriesComponent } from './pages/berries/berries.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'pokemon/:name', component: PokemonComponent }
+  { path: 'pokemon/:name', component: PokemonComponent },
+  { path: 'berries', component: BerriesComponent }
 ];
