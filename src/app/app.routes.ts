bb6174e96ae11c3f6f6d85e4f52834d91a0781e9
mayrars <mayra203@gmail.com>
2024-08-16 07:07:50 -0600

    delete berries page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PokemonComponent } from './pages/pokemon/pokemon.component';
-import { BerriesComponent } from './pages/berries/berries.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'pokemon/:name', component: PokemonComponent },
-  { path: 'berries', component: BerriesComponent }
+  { path: 'pokemon/:name', component: PokemonComponent }
 ];
